b753316c6f09ea0b009af622738894428dacd890
giansalex <giansalex@gmail.com>
2018-02-28 12:22:50 -0500

    reference product model

--- main.go
+++ main.go
@@ -3,28 +3,27 @@ package main
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"./model"
 )
-type Product struct {
-	gorm.Model
-	Code string
-	Price uint
-} 
+
+const DatabaseType = "sqlite3"
+const DatabasePath = "test.db"
 
 func main()  {
-	db, err := gorm.Open("sqlite3", "test.db")
+	db, err := gorm.Open(DatabaseType, DatabasePath)
 	if err != nil {
 		panic("failed to connect database")
 	}
 	defer db.Close()
 
 	// Migrate the schema
-	db.AutoMigrate(&Product{})
+	db.AutoMigrate(&model.Product{})
 
 	// Create
-	db.Create(&Product{Code: "L1212", Price: 1000})
+	db.Create(&model.Product{Code: "L1212", Price: 1000})
 
 	// Read
-	var product Product
+	var product model.Product
 	db.First(&product, 1) // find product with id 1
 	db.First(&product, "code = ?", "L1212") // find product with code l1212
 
